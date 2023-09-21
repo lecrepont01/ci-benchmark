076a2edc4265c5c4ac10b608b2bb553829617a31
leoecrepont <leo.ecrepont@mergify.com>
2023-09-21 15:44:10 +0200

    update mergify

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,13 @@
 def test_success():
+    print("test pycharm error")
     pass
 
 
 def test_failure():
     assert 2 > 1
+
+
+
+if __name__ == "__main__":
+    print("test bp")
+    test_success()
